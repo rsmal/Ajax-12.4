1c19439b09b4d4bd96ba97ee1fb4e8608b4842a5
Robert Smalarz <smalarz.robert@gmail.com>
2018-07-25 14:05:23 +0200

    task 12.4 finished

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var url = 'http://api.icndb.com/jokes/random';
+var paragraph = document.getElementById('joke');
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function () {
+	getJoke();
+});
+
+function getJoke() {
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function () {
+		let response = JSON.parse(xhr.response);
+		paragraph.innerHTML = response.value.joke;
+	});
+	xhr.send();
+}
